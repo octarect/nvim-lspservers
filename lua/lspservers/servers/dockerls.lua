c1e11b41e8e97599e9092e17482f8c4df2ea3b29
Ryota Kota <ryota.kota@member.fsf.org>
2021-08-11 15:39:35 +0900

    add dockerls

---
+++ lua/lspservers/servers/dockerls.lua
@@ -0,0 +1,10 @@
+local server = require'lspservers/server'
+
+return server.new({
+  name = 'dockerls',
+  cmd = { './node_modules/.bin/docker-langserver', '--stdio' },
+  installer = [[
+    set -e
+    npm install dockerfile-language-server-nodejs
+  ]],
+})

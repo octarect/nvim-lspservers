b3f50ecd84022134472cac488fcf7668c6470895
Ryota Kota <ryota.kota@member.fsf.org>
2021-08-11 15:39:12 +0900

    add tsserver

---
+++ lua/lspservers/servers/tsserver.lua
@@ -0,0 +1,10 @@
+local server = require'lspservers/server'
+
+return server.new{
+  name = 'tsserver',
+  cmd = { './node_modules/.bin/typescript-language-server', '--stdio' },
+  installer = [[
+    set -e
+    npm install typescript typescript-language-server
+  ]]
+}

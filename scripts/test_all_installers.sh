de42265a4b43855c023affc32c807714e9bbd991
Ryota Kota <ryota.kota@member.fsf.org>
2021-08-26 13:01:24 +0900

    add editorconfig

--- scripts/test_all_installers.sh
+++ scripts/test_all_installers.sh
@@ -5,12 +5,12 @@ set -e
 root_dir="$(cd $(dirname ${BASH_SOURCE:-$0})/.. && pwd)"
 
 nvim_exec() {
-    nvim --headless -u $root_dir/tests/minimal_init.vim -c "$1" -c q 2>&1
+  nvim --headless -u $root_dir/tests/minimal_init.vim -c "$1" -c q 2>&1
 }
 
 servers=("$(nvim_exec "echo join(keys(luaeval(\"require('lspservers/alias').available()\")), ' ')")")
 
 for server in $servers; do
-    echo "- - - - - - - - - -"
-    bash $root_dir/scripts/test_installer.sh $server
+  echo "- - - - - - - - - -"
+  bash $root_dir/scripts/test_installer.sh $server
 done
--- scripts/test_installer.sh
+++ scripts/test_installer.sh
@@ -7,20 +7,20 @@ target_server="${1:-vimls}"
 root_dir="$(cd $(dirname ${BASH_SOURCE:-$0})/.. && pwd)"
 
 nvim_exec() {
-    nvim --headless -u $root_dir/tests/minimal_init.vim -c "$1" -c q 2>&1
+  nvim --headless -u $root_dir/tests/minimal_init.vim -c "$1" -c q 2>&1
 }
 
 contains() {
-    list="$1"
-    value="$2"
-    printf '%s\n' $list | grep -qx "$value" 2>&1 >/dev/null
+  list="$1"
+  value="$2"
+  printf '%s\n' $list | grep -qx "$value" 2>&1 >/dev/null
 }
 
 servers=("$(nvim_exec "echo join(keys(luaeval(\"require('lspservers/alias').available()\")), ' ')")")
 
 if ! contains "$servers" "$target_server"; then
-    echo 'the server you specified cannot be found.'
-    exit 1
+  echo 'the server you specified cannot be found.'
+  exit 1
 fi
 
 installation_path="/opt/test/$target_server"
@@ -30,7 +30,7 @@ script="$(nvim_exec "echo v:lua.require('lspservers/servers').$target_server.ins
 cmd="$(nvim_exec "echo v:lua.require('lspservers/servers').$target_server.cmd[0]")"
 
 if [ -d $installation_path ]; then
-    rm -rf $installation_path
+  rm -rf $installation_path
 fi
 mkdir -p $installation_path
 cd $installation_path
@@ -44,10 +44,10 @@ echo "Executing $script_path..."
 $script_path
 echo "Check whether language server is successfully installed or not"
 if [ -f "$cmd" ]; then
-    echo "OK"
-    exit 0
+  echo "OK"
+  exit 0
 else
-    echo "NG"
-    exit 1
+  echo "NG"
+  exit 1
 fi
 

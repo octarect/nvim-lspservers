045ad2813a59d12f8b253272402ec2ee688a564d
Ryota Kota <ryota.kota@member.fsf.org>
2021-09-22 13:26:07 +0900

    detect platform in installer of sumneko_lua

--- lua/lspservers/servers/sumneko_lua.lua
+++ lua/lspservers/servers/sumneko_lua.lua
@@ -1,54 +1,52 @@
 local server = require 'lspservers/server'
 
-local platform = 'Linux'
-if vim.fn.has 'mac' == 1 then
-  platform = 'macOS'
-end
+return server.new {
+  name = 'sumneko_lua',
+  cmd = { './sumneko-lua-language-server' },
+  installer = [[
+  set -e
+  os=$(uname -s | tr "[:upper:]" "[:lower:]")
+  case $os in
+  linux)
+    platform=Linux
+    ;;
+  darwin)
+    platform=macOS
+    ;;
+  esac
 
-local installer = string.format(
-  [[
-set -e
-platform='%s'
-
-if [ "$CI" = "true" ]; then
-  # In GitHub Actions, some VM may have exceeded rate limit of GitHub API
-  version="2.3.6"
-else
-  # get latest version
-  latest_api_url="https://api.github.com/repos/sumneko/vscode-lua/releases/latest"
-  response="$(curl -s "$latest_api_url")"
-  version="$(echo "$response" | grep "tag_name" | sed -E 's/\ *"tag_name":\ *"v([^"]+)".*/\1/')"
-  if [ -z "$version" ]; then
-    echo "Unexpected response from github api:"
-    echo "$response"
-    exit 1
+  if [ "$CI" = "true" ]; then
+    # In GitHub Actions, some VM may have exceeded rate limit of GitHub API
+    version="2.3.6"
+  else
+    # get latest version
+    latest_api_url="https://api.github.com/repos/sumneko/vscode-lua/releases/latest"
+    response="$(curl -s "$latest_api_url")"
+    version="$(echo "$response" | grep "tag_name" | sed -E 's/\ *"tag_name":\ *"v([^"]+)".*/\1/')"
+    if [ -z "$version" ]; then
+      echo "Unexpected response from github api:"
+      echo "$response"
+      exit 1
+    fi
   fi
-fi
-echo "version v$version will be installed. (CI=${CI:-false})"
+  echo "version v$version will be installed. (CI=${CI:-false})"
 
-release_url="https://github.com/sumneko/vscode-lua/releases/download/v$version/lua-$version.vsix"
-curl -L "$release_url" -o tmp.vsix
-unzip tmp.vsix
-rm tmp.vsix
+  release_url="https://github.com/sumneko/vscode-lua/releases/download/v$version/lua-$version.vsix"
+  curl -L "$release_url" -o tmp.vsix
+  unzip tmp.vsix
+  rm tmp.vsix
 
-server_dir="$(cd ./extension/server; pwd)"
-luals_cmd="$server_dir/bin/$platform/lua-language-server"
+  server_dir="$(cd ./extension/server; pwd)"
+  luals_cmd="$server_dir/bin/$platform/lua-language-server"
 
-chmod +x "$luals_cmd"
+  chmod +x "$luals_cmd"
 
 cat <<EOF > ./sumneko-lua-language-server
 $luals_cmd -E -e LANG=en $server_dir/main.lua \$*
 EOF
 
-chmod +x ./sumneko-lua-language-server
-]],
-  platform
-)
-
-return server.new {
-  name = 'sumneko_lua',
-  cmd = { './sumneko-lua-language-server' },
-  installer = installer,
+  chmod +x ./sumneko-lua-language-server
+  ]],
   auto_config = function()
     return {
       settings = {
